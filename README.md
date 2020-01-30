365f683f88cad59795316cdd79e01dccdebc94f0
Eduardo Neves <eduardomag765@gmail.com>
2020-01-30 18:22:19 -0300

    README

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ## Emplyee application
 
-Aplicativo que devolve a lista de funcionários por setor. Desenvolvido para teste capgemini.
+Aplicativo que devolve a lista de funcionários por setor. Desenvolvido para teste capgemini. Para executar a aplicação siga os passos:
 
-- Primeiro passo: .
+- Primeiro passo: Importe a estrutura do banco de dados disposta em storage/polivalencia.sql, para o ambiente MySQL.
+- Em public/index.php excute os comandos $db->createtables() e $db->insert(), sendo o primeiro para criação das tabelas e o segundo para inserção dos dados;
+- Execute "php -S localhost:8000 -t public Server.php" no prompt de comando para executar na porta 8000.
+- Certifique-se de ter o serviços de banco de dados MySQL rodando na maquina, para modificar o acesso, edite o arquivo app/Factory/Database.php.
+- A página vai exibir os funcionários cadastrados no setor de São Paulo e que estão alocados a mais de 1 mês e logo abaixo uma lista de todos os funcionários.
--- storage/polivalencia.sql
+++ storage/polivalencia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 30-Jan-2020 às 15:32
+-- Generation Time: 30-Jan-2020 às 16:48
 -- Versão do servidor: 5.7.26
 -- versão do PHP: 7.3.5
 
@@ -36,21 +36,7 @@ CREATE TABLE IF NOT EXISTS `funcionarios` (
   `cidade` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `telefone` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Extraindo dados da tabela `funcionarios`
---
-
-INSERT INTO `funcionarios` (`id`, `nome`, `data_nascimento`, `cidade`, `telefone`) VALUES
-(1, 'Hermina Morissette', '23/12/1990', 'Belém', '(91) 98888-8888'),
-(2, 'Dr. Isom Considine IV', '23/12/1990', 'Belém', '(91) 98888-8888'),
-(3, 'Ellen Lindgren', '23/12/1990', 'Belém', '(91) 98888-8888'),
-(4, 'Donnell West', '23/12/1990', 'Belém', '(91) 98888-8888'),
-(5, 'Joanie Greenholt', '23/12/1990', 'Belém', '(91) 98888-8888'),
-(6, 'Prof. Alford Bogan', '23/12/1990', 'Belém', '(91) 98888-8888'),
-(7, 'Landen Kerluke Sr.', '23/12/1990', 'Belém', '(91) 98888-8888'),
-(8, 'Israel Kuphal', '23/12/1990', 'Belém', '(91) 98888-8888');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -64,24 +50,21 @@ CREATE TABLE IF NOT EXISTS `lotacao` (
   `funcionarios_id` int(11) NOT NULL,
   `postos_de_trabalho_id` int(11) NOT NULL,
   `tempo_de_alocacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `validade_de_alocacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
   KEY `funcionarios_id` (`funcionarios_id`),
   KEY `postos_de_trabalho_id` (`postos_de_trabalho_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Extraindo dados da tabela `lotacao`
+-- Acionadores `lotacao`
 --
-
-INSERT INTO `lotacao` (`id`, `funcionarios_id`, `postos_de_trabalho_id`, `tempo_de_alocacao`) VALUES
-(1, 1, 1, '2019-12-29 17:10:51'),
-(2, 2, 1, '2019-12-25 12:48:22'),
-(3, 3, 1, '2020-01-30 12:48:22'),
-(4, 4, 2, '2020-01-30 12:48:22'),
-(5, 5, 2, '2019-12-17 12:48:22'),
-(6, 6, 2, '2020-01-30 12:48:22'),
-(7, 7, 2, '2020-01-30 12:48:22'),
-(8, 8, 2, '2020-01-30 12:48:22');
+DROP TRIGGER IF EXISTS `validadeAlocacao`;
+DELIMITER $$
+CREATE TRIGGER `validadeAlocacao` BEFORE INSERT ON `lotacao` FOR EACH ROW SET NEW.validade_de_alocacao = NEW.validade_de_alocacao + INTERVAL 6 MONTH
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -95,15 +78,7 @@ CREATE TABLE IF NOT EXISTS `postos_de_trabalho` (
   `setor` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `nome` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Extraindo dados da tabela `postos_de_trabalho`
---
-
-INSERT INTO `postos_de_trabalho` (`id`, `setor`, `nome`) VALUES
-(1, 'São Paulo', 'Alverta Krajcik'),
-(2, 'Belo Horizonte', 'Abigayle Ratke');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
